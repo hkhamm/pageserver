b5f00cc64cacc909dd9e6acbb0b298cb415c8beb
Keith Hamm <hkhamm@gmail.com>
2015-09-29 11:57:25 -0700

    first commit

---
+++ pageserve_skel.py
@@ -0,0 +1,122 @@
+"""
+Socket programming in Python
+  as an illustration of the basic mechanisms of a web server.
+
+  Based largely on https://docs.python.org/3.4/howto/sockets.html
+  This trivial implementation is not robust:  We have omitted decent
+  error handling and many other things to keep the illustration as simple
+  as possible.
+
+  FIXME:
+  Currently this program always serves an ascii graphic of a cat.
+  Change it to serve files if they end with .html and are in the current directory
+"""
+
+import socket    # Basic TCP/IP communication on the internet
+import random    # To pick a port at random, giving us some chance to pick a port not in use
+import _thread   # Response computation runs concurrently with main program
+import glob
+import os
+
+
+def listen(portnum):
+    """
+    Create and listen to a server socket.
+    Args:
+       portnum: Integer in range 1024-65535; temporary use ports
+           should be in range 49152-65535.
+    Returns:
+       A server socket, unless connection fails (e.g., because
+       the port is already in use).
+    """
+    # Internet, streaming socket
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind to port and make accessible from anywhere that has our IP address
+    serversocket.bind(('', portnum))
+    serversocket.listen(1)    # A real server would have multiple listeners
+    return serversocket
+
+def serve(sock, func):
+    """
+    Respond to connections on sock.
+    Args:
+       sock:  A server socket, already listening on some port.
+       func:  a function that takes a client socket and does something with it
+    Returns: nothing
+    Effects:
+        For each connection, func is called on a client socket connected
+        to the connected client, running concurrently in its own thread.
+    """
+    while True:
+        print("Attempting to accept a connection on {}".format(sock))
+        (clientsocket, address) = sock.accept()
+        _thread.start_new_thread(func, (clientsocket,))
+
+
+CAT = """
+     ^ ^
+   =(   )=
+   """
+
+
+def respond(sock):
+    """
+    Respond (only) to GET
+
+    """
+    sent = 0
+    request = sock.recv(1024)  # We accept only short requests
+    request = str(request, encoding='utf-8', errors='strict')
+    print("\nRequest was {}\n".format(request))
+
+    parts = request.split()
+
+    # TODO parse parts to find out what page is requested
+    # if page exists read it and transmit page
+    # if page does not exist transmit error message
+
+    if (len(parts) > 1 and parts[0] == "GET" and "~" not in parts[1] and
+           ".." not in parts[1] and "//" not in parts[1]):
+        transmit("HTTP/1.0 200 OK\n\n", sock)
+
+        print(parts[1])
+
+        f = open('trivia.html', 'r')
+        msg = read_file(f)
+
+        transmit(msg, sock)
+    else:
+        transmit("\nI don't handle this request: {}\n".format(request), sock)
+
+    sock.close()
+
+    return
+
+def transmit(msg, sock):
+    """It might take several sends to get the whole buffer out"""
+    sent = 0
+    while sent < len(msg):
+        buff = bytes( msg[sent: ], encoding="utf-8")
+        sent += sock.send( buff )
+
+def read_file(file):
+    msg = ""
+    for line in file:
+        msg = msg + line
+    return msg
+
+def check_dir(file):
+    for f in os.listdir("./"):
+        if f == file:
+            return True
+        else:
+            return False
+
+def main():
+    port = 80 # random.randint(5000,8000)
+    sock = listen(port)
+    print("Listening on port {}".format(port))
+    print("Socket is {}".format(sock))
+    serve(sock, respond)
+
+main()
